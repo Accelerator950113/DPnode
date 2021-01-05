38dfa85b34110e53aba5b9583d6c8777705c2870
Qi Bao <20110240002@fudan.edu.cn>
2021-01-05 16:29:20 +0800

    Add files via upload

--- undirExp/doExp.sh
+++ undirExp/doExp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-for var in Karate Dolphins Netscience Celegans Diseasome GridMouse CrimeMoreno WikiVote Reed98 EmailUniv GridPlant Yeast Hamster USFCA72 NipsEgo GridWorm GrQc USgrid Erdos992 Bcspwr10 Reality PagesGovernment WikiElec Dmela HepPh Anybeat PagesCompany AstroPh CondMat Gplus ;do
+for var in Karate Dolphins Celegans Diseasome GridMouse CrimeMoreno WikiVote Reed98 EmailUniv GridPlant Yeast Hamster USFCA72 NipsEgo GridWorm GrQc USgrid Erdos992 Advogato Bcspwr10 Reality PagesGovernment WikiElec Dmela HepPh Anybeat PagesCompany AstroPh CondMat Gplus ;do
     julia -O3 Main.jl $var
 done
